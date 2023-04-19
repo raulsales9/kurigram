60bfe3ec550d825290e8c65e0af645999e6ddfeb
RaulSales <raulsahe@protonmail.com>
2023-04-19 18:51:28 +0200

    estructure:compoents:views

--- kurigram_front/src/app/app.module.ts
+++ kurigram_front/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ViewsComponent } from './views/views.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { EventsComponent } from './views/events/events.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { SigninComponent } from './views/signin/signin.component';
+import { ProfileComponent } from './views/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ViewsComponent,
+    ContactComponent,
+    EventsComponent,
+    HomeComponent,
+    LoginComponent,
+    SigninComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
