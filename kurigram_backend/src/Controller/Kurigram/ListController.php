708c7f05aeab3e87bb61e777d0696857453d7baf
RaulSales <raulsahe@protonmail.com>
2023-04-19 14:51:27 +0000

    controllers:maked and his respective templates

---
+++ kurigram_backend/src/Controller/Kurigram/ListController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Kurigram;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListController extends AbstractController
+{
+    #[Route('/kurigram/list', name: 'app_list')]
+    public function index(): Response
+    {
+        return $this->render('kurigram/listKurigram.html.twig', [
+            'controller_name' => 'ListController',
+        ]);
+    }
+}
