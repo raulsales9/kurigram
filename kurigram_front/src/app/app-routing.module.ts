299151632d130ce5d98148302edc243c631f8fba
RaulSales <raulsahe@protonmail.com>
2023-04-23 19:25:08 +0200

    angular: estructure and pushing all of the work

--- kurigram_front/src/app/app-routing.module.ts
+++ kurigram_front/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { EventsComponent } from './views/events/events.component';
+import { LoginComponent } from './views/login/login.component';
+import { SigninComponent } from './views/signin/signin.component';
+import { ProfileComponent } from './views/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'profile', component: ProfileComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
